0f9d056ab6bf94063c01c36ba941d0fbffe9071f
Brian Port <bport79@yahoo.com>
2017-08-10 17:08:08 -0400

    finished question loops

--- WeatherTrivia/Program.cs
+++ WeatherTrivia/Program.cs
@@ -7,7 +7,7 @@ namespace WeatherTrivia
         static void Main(string[] args)
         { 
 
-            int correctanswer = 0;
+            //int correctanswer = 0;
             int correctanswer1sttry = 0;
 
             //Greeting
@@ -16,17 +16,18 @@ namespace WeatherTrivia
             Console.WriteLine("Please enter your name:");
             string name = Console.ReadLine();
             Console.WriteLine("Let's get started " + name + "!");
-            System.Threading.Thread.Sleep(1500);
+            System.Threading.Thread.Sleep(1000);
 
             int loopcounter = 0;
             bool wascorrect = false;
+
             do {
                 //Question 1
                 Console.WriteLine("\nWhat is the \"standard\" barometric pressure at sea level in inches mercury?");
-                Console.WriteLine("\na:\t49.94");
-                Console.WriteLine("b:\t19.91");
-                Console.WriteLine("c:\t39.93");
-                Console.WriteLine("d:\t29.92");//correct answer
+                Console.WriteLine("\n\ta: 49.94");
+                Console.WriteLine("\tb: 19.91");
+                Console.WriteLine("\tc: 39.93");
+                Console.WriteLine("\td: 29.92");//correct answer
 
                 string answer1 = "d";
                 string playerInput1;
@@ -35,7 +36,7 @@ namespace WeatherTrivia
                 if (playerInput1 == answer1)
                 {
                     Console.WriteLine("\nThat is correct!");
-                    correctanswer++;
+                    //correctanswer++;
                     wascorrect = true;
                     if (loopcounter == 0)
                     {
@@ -46,84 +47,123 @@ namespace WeatherTrivia
                 else
                 {
                     Console.WriteLine("\nThat is incorrect, please try again");
-
                 }
                 loopcounter++;
+                System.Threading.Thread.Sleep(1000);
             } while (!wascorrect);
 
 
-            System.Threading.Thread.Sleep(1500);
+            System.Threading.Thread.Sleep(1000);
 
-            //Question 2
-            Console.WriteLine("\nWhat is the force that deflects air to the right in the northern hemisphere?");
-            Console.WriteLine("\na:\tGradient Force");
-            Console.WriteLine("b:\tCoriolis Force");//correct answer
-            Console.WriteLine("c:\tNorthern Force");
-            Console.WriteLine("d:\tSouthern Force");
+            bool wascorrect1 = false;
+            int loopcounter1 = 0;
 
-            string answer2 = "b";
-            string playerInput2;
-
-            playerInput2 = Console.ReadLine();
-            if (playerInput2 == answer2)
+            do
             {
-                Console.WriteLine("\nThat is correct!");
-                correctanswer++;
+                //Question 2
+                Console.WriteLine("\nWhat is the force that deflects air to the right in the northern hemisphere?");
+                Console.WriteLine("\n\ta: Gradient Force");
+                Console.WriteLine("\tb: Coriolis Force");//correct answer
+                Console.WriteLine("\tc: Northern Force");
+                Console.WriteLine("\td: Southern Force");
 
-            }
-            else
-            {
-                Console.WriteLine("\nThat is incorrect, the right answer is {0}", answer2);
-            }
+                string answer2 = "b";
+                string playerInput2;
 
-            System.Threading.Thread.Sleep(1500);
+                playerInput2 = Console.ReadLine();
+                if (playerInput2 == answer2)
+                {
+                    Console.WriteLine("\nThat is correct!");
+                    //correctanswer++;
+                    wascorrect1 = true;
+                    if (loopcounter1 == 0)
+                    {
+                        correctanswer1sttry++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nThat is incorrect, please try again.");                    
+                }
+                loopcounter1++;
+                System.Threading.Thread.Sleep(1000);
+            } while (!wascorrect1);
+
+            System.Threading.Thread.Sleep(1000);
+
+            bool wascorrect2 = false;
+            int loopcounter2 = 0;
 
             //Question 3
-            Console.WriteLine("\nIn what layer of the atmosphere would you find the Jet Stream?");
-            Console.WriteLine("\na:\tTroposphere");
-            Console.WriteLine("b:\tMesosphere");
-            Console.WriteLine("c:\tExosphere");
-            Console.WriteLine("d:\tTropopause");//correct answer
-
-            string answer3 = "d";
-            string playerInput3;
-
-            playerInput3 = Console.ReadLine();
-            if(playerInput3 == answer3)
+            do
             {
-                Console.WriteLine("\nThat is Correct!");
-                correctanswer++;
-            }
-            else
+                Console.WriteLine("\nIn what layer of the atmosphere would you find the Jet Stream?");
+                Console.WriteLine("\n\ta: Troposphere");
+                Console.WriteLine("\tb: Mesosphere");
+                Console.WriteLine("\tc: Exosphere");
+                Console.WriteLine("\td: Tropopause");//correct answer
+
+                string answer3 = "d";
+                string playerInput3;
+
+                playerInput3 = Console.ReadLine();
+                if (playerInput3 == answer3)
+                {
+                    Console.WriteLine("\nThat is Correct!");
+                    wascorrect2 = true;
+                    //correctanswer++;
+                    if (loopcounter2 == 0)
+                    {
+                        correctanswer1sttry++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nThat is incorrect, please try again");
+                }
+                loopcounter2++;
+                System.Threading.Thread.Sleep(1000);
+            } while (!wascorrect2);
+
+            System.Threading.Thread.Sleep(1000);
+
+            bool wascorrect3 = false;
+            int loopcounter3 = 0;
+
+            do
             {
-                Console.WriteLine("\nThat is incorrect, the right answer is {0}", answer3);
-            }
 
-            System.Threading.Thread.Sleep(1500);
+                //question 4
+                Console.WriteLine("\nAn Elongated area of high pressure is called a/an");
+                Console.WriteLine("\n\ta: Ridge");//correct answer
+                Console.WriteLine("\tb: Trough");
+                Console.WriteLine("\tc: Front");
+                Console.WriteLine("\td: Air mass");
 
-            //question 4
-            Console.WriteLine("\nAn Elongated area of high pressure is called a/an");
-            Console.WriteLine("\na:\tRidge");//correct answer
-            Console.WriteLine("b:\tTrough");
-            Console.WriteLine("c:\tFront");
-            Console.WriteLine("d:\tAir mass");
+                string answer4 = "a";
+                string playerInput4;
 
-            string answer4 = "a";
-            string playerInput4;
+                playerInput4 = Console.ReadLine();
+                if (playerInput4 == answer4)
+                {
+                    Console.WriteLine("\nThat is Correct!");
+                    wascorrect3 = true;
+                    //correctanswer++;
+                    if (loopcounter3 == 0)
+                    {
+                        correctanswer1sttry++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nThat is incorrect, please try again");
+                }
+                loopcounter3++;
+                System.Threading.Thread.Sleep(1000);
+            } while (!wascorrect3);
 
-            playerInput4 = Console.ReadLine();
-            if(playerInput4 == answer4)
-            {
-                Console.WriteLine("\nThat is Correct!");
-                correctanswer++;
-            }
-            else
-            {
-                Console.WriteLine("\nThat is incorrect, the right answer is {0}", answer4);
-            }
-
-            System.Threading.Thread.Sleep(1500);
-            Console.WriteLine("\nThanks for playing " + name + ", you got {0} percent correct.", correctanswer * (25));
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("\nThanks for playing " + name + ", you got {0} correct on the first try.", correctanswer1sttry);
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
